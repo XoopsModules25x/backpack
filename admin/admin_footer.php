5251cdec333876fd9d9544449219bf1295974cb2
Michael Beck <mambax7@users.noreply.github.com>
2020-11-06 20:06:00 -0500

    Update admin/admin_footer.php
    Co-authored-by: ZySpec Development <zyspec@users.noreply.github.com>

--- admin/admin_footer.php
+++ admin/admin_footer.php
@@ -15,7 +15,7 @@ echo '<div class="adminfooter">'
      . "\n"
      . '    <a href="https://www.xoops.org" rel="external"><img src="'
      . $pathIcon32
-     . '/xoopsmicrobutton.gif" alt="Xoops" title="Xoops"></a>'
+     . '/xoopsmicrobutton.gif" alt="XOOPS" title="XOOPS"></a>'
      . "\n"
      . '  </div>'
      . "\n"
